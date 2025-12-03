dc3346f0c0921b03237f80177141259b739694bd
noe FLORENCE <noe@keflo.org>
2025-12-03 01:02:12 +0100

    omg le robot avec la pince roule nikel

--- launch/auto_explore.launch.py
+++ launch/auto_explore.launch.py
@@ -1,63 +1,98 @@
 import os
+import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, GroupAction, AppendEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-from launch.actions import GroupAction
-from launch_ros.actions import PushRosNamespace
+from launch_ros.actions import Node, PushRosNamespace
+
 
 def generate_launch_description():
-
-    env_model = SetEnvironmentVariable(name='TURTLEBOT3_MODEL', value='waffle')
-    env_lidar = SetEnvironmentVariable(name='LDS_MODEL', value='LDS-01')
-    
-    env_gl = SetEnvironmentVariable(name='LIBGL_ALWAYS_SOFTWARE', value='1')
-
+    urdf_file_name = 'turtlebot3_ultimate.urdf'
     use_sim_time = 'true'
-    
-    # Chemins des packages standards
+
+    pkg_tb3_autonomy = get_package_share_directory('tb3_autonomy')
     pkg_tb3_gazebo = get_package_share_directory('turtlebot3_gazebo')
     pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
-    
-    nav2_params = os.path.join(pkg_nav2_bringup, 'params', 'nav2_params.yaml')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_stereo_image_proc = get_package_share_directory('stereo_image_proc')
+    pkg_tb3_description = get_package_share_directory('turtlebot3_description')
+
+
+    gazebo_model_path = os.path.join(pkg_tb3_description, '..') + ':' + os.path.join(pkg_tb3_autonomy, '..')
+
+    set_gazebo_model_path = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=gazebo_model_path
+    )
+
+    env_lidar = SetEnvironmentVariable(name='LDS_MODEL', value='LDS-01')
+    env_gl = SetEnvironmentVariable(name='LIBGL_ALWAYS_SOFTWARE', value='0')
+
+    urdf_path = os.path.join(pkg_tb3_autonomy, 'urdf', urdf_file_name)
+
+    doc = xacro.process_file(urdf_path)
+    robot_desc = doc.toxml()
+
+    robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'use_sim_time': True,
+            'robot_description': robot_desc
+        }]
+    )
+
+    spawn_entity_cmd = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'my_custom_waffle',
+            '-x', '-2.0',
+            '-y', '-0.5',
+            '-z', '0.15'
+        ],
+        output='screen'
+    )
+
+    world_path = os.path.join(pkg_tb3_gazebo, 'worlds', 'turtlebot3_world.world')
 
     gazebo_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_tb3_gazebo, 'launch', 'turtlebot3_world.launch.py')
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
         ),
-        launch_arguments={
-            'use_sim_time': use_sim_time,
-            'x_pose': '-2.0',
-            'y_pose': '-0.5'
-        }.items()
+        launch_arguments={'world': world_path}.items()
     )
 
-    # SLAM (Cartographie en temps réel)
+    #nav2_params = os.path.join(pkg_nav2_bringup, 'params', 'nav2_params.yaml')
+    pkg_tb3_autonomy = get_package_share_directory('tb3_autonomy')
+    
+    nav2_params = os.path.join(pkg_tb3_autonomy, 'params', 'my_nav2_params.yaml')
     slam_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_nav2_bringup, 'launch', 'slam_launch.py')
         ),
         launch_arguments={
-            'use_sim_time': use_sim_time, 
-            'params_file': nav2_params
+            'use_sim_time': use_sim_time,
+            'params_file': nav2_params,
+            'transform_timeout': '0.5'
         }.items()
     )
 
-    # Navigation
     navigation_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_nav2_bringup, 'launch', 'navigation_launch.py')
         ),
         launch_arguments={
-            'use_sim_time': use_sim_time, 
+            'use_sim_time': use_sim_time,
             'params_file': nav2_params,
             'log_level': 'Fatal'
         }.items()
     )
 
-    
-    # Explore Lite
     explore_cmd = Node(
         package='explore_lite',
         executable='explore',
@@ -77,25 +112,24 @@ def generate_launch_description():
             'min_frontier_size': 0.75,
         }]
     )
-    pkg_stereo_image_proc = get_package_share_directory('stereo_image_proc')
 
     stereo_proc = GroupAction(
         actions=[
-            PushRosNamespace('oakd'), # Tout se passera dans /oakd/...
+            PushRosNamespace('oakd'),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
                     os.path.join(pkg_stereo_image_proc, 'launch', 'stereo_image_proc.launch.py')
                 ),
                 launch_arguments={
-                    'approximate_sync': 'True', # Indispensable pour la simu
+                    'approximate_sync': 'True',
                     'use_sim_time': 'True',
-                    'left_namespace': 'left', 
+                    'left_namespace': 'left',
                     'right_namespace': 'right',
                 }.items()
             )
         ]
     )
-    
+
     rviz_cmd = Node(
         package='rviz2',
         executable='rviz2',
@@ -113,10 +147,12 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        env_model,
+        set_gazebo_model_path,
         env_lidar,
         env_gl,
         gazebo_cmd,
+        robot_state_publisher_cmd,
+        spawn_entity_cmd,
         slam_cmd,
         navigation_cmd,
         explore_cmd,
