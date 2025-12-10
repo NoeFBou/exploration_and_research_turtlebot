43b550f7edc321ea7b757d70cb7dfff85fd014a7
Kitsunro <Louismalma@gmail.com>
2025-12-10 08:51:55 +0100

    Remove turtlebot3_ultimate1.urdf file and its associated components

--- launch/mission.launch.py
+++ launch/mission.launch.py
@@ -1,4 +1,4 @@
-import os
+""" import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
@@ -85,3 +85,4 @@ def generate_launch_description():
         explore_cmd,
         supervisor_cmd
     ])
+ """
