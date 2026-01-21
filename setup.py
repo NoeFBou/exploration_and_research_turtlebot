e47bf8e874210e069dfdc62d103bc0e3599b90e1
NoeFBou <102726520+NoeFBou@users.noreply.github.com>
2026-01-21 22:12:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name=package_name,
     version="0.0.1",
     packages=[package_name, package_name + '.behaviors', package_name + '.nodes'],    data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/ament_index/ressource_index/packages", ["ressource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
         (os.path.join("share", package_name, "worlds"), glob("worlds/*.sdf")),
